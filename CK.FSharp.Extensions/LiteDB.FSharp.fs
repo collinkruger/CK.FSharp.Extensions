8f7fe83e3ff2b0e55dd9b7a35cd17887c958529e
Collin Kruger <collin.kruger@solve100.com>
2019-10-16 12:29:01 -0500

    Add LiteDB.FSharp.fs Add LiteSingleItemCollection<'T>

---
+++ CK.FSharp.Extensions/LiteDB.FSharp.fs
@@ -0,0 +1,30 @@
+﻿/// Extensions To LiteDB.FSharp https://github.com/Zaid-Ajaj/LiteDB.FSharp
+module LiteDB.FSharp
+open LiteDB
+
+/// Type Used Internally By Collection Functions.
+/// There Shouldn't Be A Reason To Consume This Publicly.
+/// This Type Must Be Public For The Serializer To Behave Correctly.
+type IDed<'T> = {
+    Id: int
+    Value: 'T
+}
+
+type LiteSingleItemCollection<'T>(col: LiteCollection<IDed<'T>>) =
+    member _.get () =
+        col.FindAll()
+           |> Seq.map (fun x -> x.Value)
+           |> Seq.tryHead
+
+    member _.set (value: 'T) =
+        col.Upsert({ Id = 1; Value = value })
+           |> ignore
+
+    member _.delete () =
+        col.Delete(BsonValue 1)
+           |> ignore
+
+type LiteDatabase with
+    member this.SingleCollection<'T>(name: string) =
+        this.GetCollection<IDed<'T>>(name)
+        |> LiteSingleItemCollection
